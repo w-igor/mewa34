fb6e3b146611976d183e454f1d0116ea71c725cd
w-igor <igor.wojslaw@gmail.com>
2026-06-29 18:14:34 +0200

    7

--- js/news.js
+++ js/news.js
@@ -1,3 +1,5 @@
-var response = await fetch("/content/news/news.json");
+var space_id = "bmp9s2ulzj14";
+var access_token = "6ro8gXkKjsvWCzMGoUwNB_q51VN9afHs7jW9WhfruR4";
+var response = await fetch("https://cdn.contentful.com/spaces/" + space_id + "/entries?content_type=news", { headers: { Authorization: "Bearer " + access_token } });
 var data = await response.json();
-document.querySelector(".news > .text").innerHTML = data.items.map(item => '<div class="news_item">' + item.body + '</div>').join("");
+document.querySelector(".news > .text").innerHTML = data.items.map(item => item.fields.text).join("");
