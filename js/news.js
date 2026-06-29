d919d3b8337a8f116c5014d4d047e2a69f555453
w-igor <igor.wojslaw@gmail.com>
2026-06-29 18:19:29 +0200

    8

--- js/news.js
+++ js/news.js
@@ -1,5 +1,8 @@
-var space_id = "bmp9s2ulzj14";
-var access_token = "6ro8gXkKjsvWCzMGoUwNB_q51VN9afHs7jW9WhfruR4";
-var response = await fetch("https://cdn.contentful.com/spaces/" + space_id + "/entries?content_type=news", { headers: { Authorization: "Bearer " + access_token } });
-var data = await response.json();
-document.querySelector(".news > .text").innerHTML = data.items.map(item => item.fields.text).join("");
+window.addEventListener("load", async () =>
+{
+    var space_id = "bmp9s2ulzj14";
+    var access_token = "6ro8gXkKjsvWCzMGoUwNB_q51VN9afHs7jW9WhfruR4";
+    var response = await fetch("https://cdn.contentful.com/spaces/" + space_id + "/entries?content_type=news", { headers: { Authorization: "Bearer " + access_token } });
+    var data = await response.json();
+    document.querySelector(".news > .text").innerHTML = data.items.map(item => item.fields.text).join(""); 
+});
